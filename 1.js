e52d30ff30bf997f886c8c51630338504e3bf476
trunglk16 <trungnguyenlk16@gmail.com>
2018-11-04 19:25:07 +0700

    Thêm title

--- 1.js
+++ 1.js
@@ -2,6 +2,13 @@ var r,c,s;
 const pi = 3.14159;
 r = prompt("Nhap ban kinh hinh tron");
 
-s = pi * r * r;
+if (r <= 0){
+    document.write("Hinh tron khong ton tai")
+}
+else {
 
-document.write("Dien tich hinh tron = " + s );
+    c = 2 * pi * r;
+    s = pi * r * r;
+
+    document.write("Dien tich hinh tron = " + s);
+}
